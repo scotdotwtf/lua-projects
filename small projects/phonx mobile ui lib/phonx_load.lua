57a1b99373194d2e45e92dc6ddcf83d10ea864b9
spec <77427029+specowos@users.noreply.github.com>
2021-12-04 12:26:01 -0600

    Update phonx_load.lua

--- small projects/phonx mobile ui lib/phonx_load.lua
+++ small projects/phonx mobile ui lib/phonx_load.lua
@@ -1,7 +1,7 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/phonx%20mobile%20ui%20lib/phonx_src.lua"))() --// load lib
+local lib = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/phonx%20mobile%20ui%20lib/phonx_src.lua"))()
 
 --// examples
-local start = lib:start("phonx") --// this makes the ui so u kinda need this
+local start = lib:start("ok") --// this makes the ui so u kinda need this
 
 local button1 = lib:newbutton("button", function() --// this is a button
     print("this button has been clicked")
