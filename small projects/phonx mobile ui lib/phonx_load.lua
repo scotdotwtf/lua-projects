41f6dc42a0207778784a26c9b70114438a4c8f7f
spec <77427029+specowos@users.noreply.github.com>
2021-12-04 14:08:48 -0600

    Update phonx_load.lua

--- small projects/phonx mobile ui lib/phonx_load.lua
+++ small projects/phonx mobile ui lib/phonx_load.lua
@@ -1,3 +1,7 @@
+_G.TopBackgroundColor = Color3.fromRGB(73, 152, 255) --// defualt is (73, 152, 255)
+_G.MainBackgroundColor = Color3.fromRGB(37, 121, 223) --// defualt is (37, 121, 223)
+_G.AllTextColor = Color3.fromRGB (255, 255, 255) --// defialt is (255, 255, 255)
+
 local lib = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/phonx%20mobile%20ui%20lib/phonx_src.lua"))()
 
 --// examples
