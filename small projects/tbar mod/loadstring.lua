419445d07bf6eeed19b8e27a9390749e97264c1b
spec <77427029+specowos@users.noreply.github.com>
2022-02-05 22:16:25 -0600

    Update loadstring.lua

--- small projects/tbar mod/loadstring.lua
+++ small projects/tbar mod/loadstring.lua
@@ -1,2 +1,2 @@
---// Enjoy * sorry for any errors btw *
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/tbar%20mod/tbar.lua',true))()
+--// MOVED https://github.com/specowos/lua-projects/tree/main/small%20projects/project:2016 MOVED //--
+loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/project%3A2016/2016raw.lua',true))()
