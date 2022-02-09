af101e0656b1c125d79c367a81121238d2be68ea
spec <77427029+specowos@users.noreply.github.com>
2022-02-08 18:21:32 -0600

    Update 2016raw.lua

--- small projects/project:2016/2016raw.lua
+++ small projects/project:2016/2016raw.lua
@@ -3043,7 +3043,7 @@ local chatico = tbar.LeftFrame.ChatIcon.Background.Icon
 local UIS = game:GetService("UserInputService")
 tbar.RightFrame.HealthBar:Destroy()
 game.RunService.Heartbeat:Connect(function()
-if game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar:IsFocused() then
+if game.Players.LocalPlayer.PlayerGui.Chat:WaitForChild("Frame").ChatBarParentFrame.Frame.BoxFrame.Frame.ChatBar:IsFocused() then
 game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.BackgroundTransparency = 0.1
 else
 game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.BackgroundTransparency = game.Players.LocalPlayer.PlayerGui.Chat.Frame.ChatBarParentFrame.Frame.BoxFrame.BackgroundTransparency
