34918a97349da8820b6de15b6376d5113c19ba85
spec <77427029+specowos@users.noreply.github.com>
2022-02-24 19:10:43 -0600

    add clasic cam parent

--- small projects/project:2016/2016raw.lua
+++ small projects/project:2016/2016raw.lua
@@ -3314,3 +3314,13 @@ end)
 end
 oldcon()
 
+local cam = workspace.CurrentCamera
+
+script.Parent.Parent.Character.Humanoid.Changed:connect(function(val)
+if script.Parent.Parent.Character.Humanoid.Sit == true then
+wait(1)
+cam.CameraSubject = game.Players.LocalPlayer.Character.Humanoid
+cam.CameraType = "Attached" 
+script:Remove()
+end 
+end)
