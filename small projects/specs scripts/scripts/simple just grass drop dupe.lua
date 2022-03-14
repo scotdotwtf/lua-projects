49f06fe92a54f868bd27139f0faecfe15690fc59
spec <77427029+specowos@users.noreply.github.com>
2022-03-14 12:47:43 -0500

    Create simple just grass drop dupe.lua

---
+++ small projects/specs scripts/scripts/simple just grass drop dupe.lua
@@ -0,0 +1,6 @@
+--// run this like 15 times
+local fart = game.Players.LocalPlayer.Character
+game.Players.LocalPlayer.Backpack.BoomBox.Parent = fart
+fart.BoomBox.Parent = workspace
+wait()
+game.Players:Chat("-re")
