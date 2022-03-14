885a1de3dcb65d8caf1324e42f2820beaeab0d1d
spec <77427029+specowos@users.noreply.github.com>
2022-03-14 12:52:07 -0500

    gonna move this

--- small projects/specs scripts/scripts/simple just grass drop dupe.lua
+++
@@ -1,6 +0,0 @@
---// run this like 15 times
-local fart = game.Players.LocalPlayer.Character
-game.Players.LocalPlayer.Backpack.BoomBox.Parent = fart
-fart.BoomBox.Parent = workspace
-wait()
-game.Players:Chat("-re")
