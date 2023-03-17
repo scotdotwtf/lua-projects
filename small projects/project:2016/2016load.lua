0dbcdf9590254433a7c2cec1811f22c0f0759e80
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:21:05 -0500

    Update 2016load.lua

--- small projects/project:2016/2016load.lua
+++ small projects/project:2016/2016load.lua
@@ -1,2 +1,2 @@
 --// Enjoy * sorry for any errors btw *
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/project%3A2016/2016raw.lua',true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/scotdotwtf/lua-projects/main/small%20projects/project%3A2016/2016raw.lua',true))()
