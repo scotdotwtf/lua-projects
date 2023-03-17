c41b42ef14231fc32ff3affd1a7f6cfa18382efa
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:22:13 -0500

    Update loadstring.lua

--- small projects/tbar mod/loadstring.lua
+++ small projects/tbar mod/loadstring.lua
@@ -1,2 +1,2 @@
---// MOVED https://github.com/specowos/lua-projects/tree/main/small%20projects/project:2016 MOVED //--
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/project%3A2016/2016raw.lua',true))()
+--// MOVED https://github.com/scotdotwtf/lua-projects/tree/main/small%20projects/project:2016 MOVED //--
+loadstring(game:HttpGet('https://raw.githubusercontent.com/scotdotwtf/lua-projects/main/small%20projects/project%3A2016/2016raw.lua',true))()
