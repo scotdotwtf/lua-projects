2983bbc6d51b65fd267c502eb3b3e2373761ca92
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:20:05 -0500

    Update phonx_load.lua

--- small projects/phonx mobile ui lib/phonx_load.lua
+++ small projects/phonx mobile ui lib/phonx_load.lua
@@ -2,7 +2,7 @@ _G.TopBackgroundColor = Color3.fromRGB(73, 152, 255) --// defualt is (73, 152, 2
 _G.MainBackgroundColor = Color3.fromRGB(37, 121, 223) --// defualt is (37, 121, 223)
 _G.AllTextColor = Color3.fromRGB (255, 255, 255) --// defialt is (255, 255, 255)
 
-local lib = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/specowos/lua-projects/main/small%20projects/phonx%20mobile%20ui%20lib/phonx_src.lua"))()
+local lib = loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/scotdotwtf/lua-projects/main/small%20projects/phonx%20mobile%20ui%20lib/phonx_src.lua"))()
 
 --// examples
 local start = lib:start("ok") --// this makes the ui so u kinda need this
